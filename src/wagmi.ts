4f195951c5527ca64f85f57fc65def1bad034e6f
vlad17451 <vlad17451@gmail.com>
2024-08-19 19:23:32 +0200

    wallet connect

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,14 +1,12 @@
 import { http, createConfig } from 'wagmi'
 import { optimism } from 'wagmi/chains'
-// import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
-import { injected } from 'wagmi/connectors'
+import { injected, walletConnect } from 'wagmi/connectors'
 
 export const config = createConfig({
   chains: [optimism],
   connectors: [
     injected(),
-    // coinbaseWallet(),
-    // walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
+    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
   ],
   transports: {
     [optimism.id]: http(),
